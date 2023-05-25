452a7f493e5d7c344aeef233ebf7a44261ddb51e
liamcrowe02 <liamcrowe2502@gmail.com>
2023-05-25 23:35:44 +0100

    Added a couple comments in css folders

--- web-portfolio/src/components/Home.js
+++ web-portfolio/src/components/Home.js
@@ -1,16 +1,61 @@
 import "./homeStyles.css";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+  const greeting = "Hi, I am Liam";
 
-    return (
-        <>
-            <p>Hi, My Name is Liam Crowe</p>
-            <h1>About Me</h1>
-            <h1>Featured Projects</h1>
-        </>
-    
-    );
-  }
-  
-  export default Home;
+  return (
+    <>
+      <div className="intro">
+        <p>{greeting}</p>
+        <div className="rotated-square">
+          <div className="square"></div>
+        </div>
+      </div>
+
+      <div>
+        <h1 className="about-heading">
+          <hr className="left-line" />
+          About Me
+          <hr className="right-line" />
+        </h1>
+      </div>
+      
+      <div>
+        <h1 className="about-heading">
+          <hr className="left-line" />
+          Experience
+          <hr className="right-line" />
+        </h1>
+      </div>
+
+      <div>
+        <h1 className="about-heading">
+          <hr className="left-line" />
+          Featured Projects
+          <hr className="right-line" />
+        </h1>
+      </div>
+
+      <div className="card">
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRs8ram-SCoTrQsgB5xrwVs326B3PpIhwKbiZv5x2Y&s'
+        alt="Project" className="card-image" />
+        <div className="card-content">
+          <h3 className="card-title">Cyber</h3>
+          <p className="card-description">aDHSAFNGIJNOANGONOANSDLNGSDNS JNGNSOGNSK
+          aDHSAFNGIJNOANGONOANSDLNGSDNS JNGNSOGNSK
+          aDHSAFNGIJNOANGONOANSDLNGSDNS JNGNSOGNSK
+          aDHSAFNGIJNOANGONOANSDLNGSDNS JNGNSOGNSK
+          aDHSAFNGIJNOANGONOANSDLNGSDNS JNGNSOGNSK
+          </p>
+          <div className="card-buttons">
+            <button className="card-button">Github</button>
+            <button className="card-button">Read More</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
